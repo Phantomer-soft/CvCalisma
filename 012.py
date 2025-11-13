e03fe614067e4b748e64758b5afcef9db16f7f1b
Phantomer-soft <kucuktas5151@hotmail.com>
2025-11-14 00:05:37 +0300

    5.Gün Trackbar+Maskeleme İşlemleri

--- 012.py
+++ 012.py
@@ -6,7 +6,7 @@ cv2.imshow("araba",araba)
 araba2=cv2.cvtColor(araba,cv2.COLOR_BGR2GRAY) #araba nesnesinin renklerini rgbye çevirip araba 2 nesnesi ürettik
 araba3=cv2.cvtColor(araba,cv2.COLOR_BGR2HSV)
 cv2.imshow("araba2",araba2)
-cv2.imshow("araba3",araba3)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
